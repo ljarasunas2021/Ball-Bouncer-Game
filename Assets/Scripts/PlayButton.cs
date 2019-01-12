bac65cf15dafd98471fc227f6ba421288dabd877
Lukas Jarasunas <ljarasunas2021@chadwickschool.org>
2019-01-11 16:58:36 -0800

    feat: Finish module 2 and lesson 8

---
+++ Assets/Scripts/PlayButton.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PlayButton : MonoBehaviour
+{
+    void Start()
+    {
+      Button start = GetComponent<Button>();
+      start.onClick.AddListener(StartGame);
+    }
+
+    void Update()
+    {
+
+    }
+
+    void StartGame () {
+      Debug.Log("starting game");
+      SceneManager.LoadScene("level1");
+    }
+}
